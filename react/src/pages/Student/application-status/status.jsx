bf376dd3cbf0a8068872a13e7c5069dcd8a73546
Tondi11 <tondimabasa@gmail.com>
2025-04-11 09:38:13 +0200

    Added pages for notifications and application status

---
+++ react/src/pages/Student/application-status/status.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../../../components/studentNavbar';
+
+const ApplicationStatus = () => {
+    // Add your logic and state management here
+
+    return (
+        <div>
+            <NavBar />
+            <h1>Application Status</h1>
+            {/* Add your UI components and status display here */}
+        </div>
+    );
+};
+
+export default ApplicationStatus;
